a45bc5ac1c348c0215341e83a1dbd73f0f0a1aa8
Himank Yadav <himankyadav@Himanks-MacBook-Pro.local>
2016-08-30 22:14:15 -0500

    v1

--- flask-demo.py
+++ flask-demo.py
@@ -1,7 +1,9 @@
 # Some cute flask app.
-
-from flask import Flask
+from twilio.rest import TwilioRestClient
+from flask import Flask, render_template, request
+from configobj import ConfigObj
 app = Flask(__name__)
+config = ConfigObj('config.ini')
 
 @app.route('/')
 @app.route('/<name>')
@@ -9,6 +11,22 @@ def home(name=None):
     return render_template('home.html',name=name)
 
 	
-@app.route('/message')
+@app.route('/message', methods=['GET','POST'])
 def message():
-    return render_template('message.html')
+    if request.method == 'GET':
+        return render_template('message.html', success=None)
+    else:
+        number = request.form['phoneNumber']
+        message = request.form['message']
+
+        account_sid = config['sid'] # Your Account SID from www.twilio.com/console
+        auth_token  = config['token']  # Your Auth Token from www.twilio.com/console
+
+        client = TwilioRestClient(account_sid, auth_token)
+
+        message = client.messages.create(body=message,
+            to="+1" + number,    # Replace with your phone number
+            from_="+17135978334") # Replace with your Twilio number        
+        return render_template('message.html', success="You sent a bloody message")
+if __name__ == '__main__':
+    app.run()
